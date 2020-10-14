ff2555f38b0c3af716c621754c8568043650623d
Jaime Portas <jaime.pg.info@gmail.com>
2020-10-14 15:03:30 +0200

    Add MS Visual Studio compile options and use \n instead of std::endl.

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,5 @@
 
 int main()
 {
-    [out = std::ref(std::cout << "Hello ")]() { out.get() << "World" << std::endl; }();
+    [out = std::ref(std::cout << "Hello ")]() { out.get() << "World!\n"; }(); // Do not use std::endl because it does a flush. See https://youtu.be/GMqQOEZYVJQ
 }
