74fdb34191b26b082788318491909d2165d0329b
Jaime Portas <jaime.pg.info@gmail.com>
2020-10-14 16:29:21 +0200

    Add ASIO library and link against it.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <functional>
 
+#include <asio.hpp>
+#include <asio/ts/buffer.hpp>
+#include <asio/ts/internet.hpp>
+
 int main()
 {
     [out = std::ref(std::cout << "Hello ")]() { out.get() << "World!\n"; }(); // Do not use std::endl because it does a flush. See https://youtu.be/GMqQOEZYVJQ
